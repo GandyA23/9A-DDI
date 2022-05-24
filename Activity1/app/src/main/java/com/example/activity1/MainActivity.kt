0815502b7da8ac6691cd923c4f9e1ed050f711b4
Gandy Avila <20193tn128@utez.edu.mx>
2022-05-24 15:46:01 -0500

    Actividad 1 Android Studio - Hello round world

---
+++ Activity1/app/src/main/java/com/example/activity1/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.activity1
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.activity1.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
