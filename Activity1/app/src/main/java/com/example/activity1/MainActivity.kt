771ba1dcff9fe1d1bd0c6f826f81c7da23da73ea
GandyA23 <avila.gandy@gmail.com>
2022-05-26 16:45:52 -0500

    Se realiza avance mini-aplicación de cronómetro

--- Activity1/app/src/main/java/com/example/activity1/MainActivity.kt
+++ Activity1/app/src/main/java/com/example/activity1/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.activity1
 
 import android.app.Activity
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
 import com.example.activity1.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
@@ -14,5 +17,25 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Consigue la vista dentro del binding para añadir funcionalidad
+        val btnPlay : Button = binding.btnPlay
+        val btnStop : Button = binding.btnStop
+        val btnReplay : Button = binding.btnReplay
+        val chronometer : Chronometer = binding.chronometer
+
+        btnPlay.setOnClickListener {
+            chronometer.base = SystemClock.elapsedRealtime()
+            chronometer.start()
+        }
+
+        btnStop.setOnClickListener {
+            chronometer.stop()
+        }
+
+        btnReplay.setOnClickListener {
+            chronometer.stop()
+            chronometer.base = SystemClock.elapsedRealtime()
+            chronometer.start()
+        }
     }
 }
