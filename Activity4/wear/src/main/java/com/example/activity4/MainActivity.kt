e2e3a8d61ef15717e1b8eb9301894212772975f8
GandyA23 <avila.gandy@gmail.com>
2022-06-14 15:49:11 -0500

    Se agrega nueva aplicación 4
    La nueva aplicación es un ejemplo de una aplicación con uso de
    diferentes vistas y funcionalidades que se pueden usar en dispositivos
    WearOs.

---
+++ Activity4/wear/src/main/java/com/example/activity4/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.activity4
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.example.activity4.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val btnBook : Button = binding.btnBook
+
+        btnBook.setOnClickListener {
+            // De mi vista (this) ve a la vista de book (MainActivityBook)
+            val intent = Intent(this, MainActivityBook::class.java)
+
+            // Pasar datos a mi MainActivity
+            // intent.putExtra("fullname", "Ávila Estrada Gandy Esaú")
+
+            // Vamos a mi vista
+            startActivity(intent)
+
+            // Mostrar errores o warnings en LogCat
+            Log.w("warning_intent", "Ha ocurrido un warning")
+
+            Log.v("error_intent", "Ha ocurrido un error")
+        }
+    }
+}
