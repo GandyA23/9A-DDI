99f682a1589b2bf4058b85496910a08513fc9e89
GandyA23 <avila.gandy@gmail.com>
2022-06-27 11:17:35 -0500

    Configuración a Firebase y testeo del mismo
    El archivo agregado a .gitignore es el archivo de configuración para Firebase (google-services.json), dejando un simple ejemplo de como es su estructura en google-services.json.example
    Se agregan dependencias a nivel proyecto y wear necesarios para la conexión con Firebase
    Se agrega un ejemplo en MainActivity de wear

--- Activity5/wear/src/main/java/com/example/activity5/MainActivity.kt
+++ Activity5/wear/src/main/java/com/example/activity5/MainActivity.kt
@@ -2,9 +2,17 @@ package com.example.activity5
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import com.example.activity5.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : Activity() {
+
+    companion object {
+        const val TAG = "TestMainActivity"
+        private val NOTES = Firebase.firestore.collection("notes")
+    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -14,5 +22,18 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Testing get a document once
+        val docRef = NOTES.document("pl2ooxOWoQbWesSJfEbN")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
     }
 }
