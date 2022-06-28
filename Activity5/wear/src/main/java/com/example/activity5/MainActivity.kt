821f5339c5412e96d497aae6f861a7609a242d8b
GandyA23 <avila.gandy@gmail.com>
2022-06-28 15:53:18 -0500

    Configuración de adapter en MainActivity
    Se configura dataBinding en gradle a nivel módulo (wear)
    Se elimina testFirebase de MainActivity
    Se obtienen todas las notas y se muestra una lista en MainActivity

--- Activity5/wear/src/main/java/com/example/activity5/MainActivity.kt
+++ Activity5/wear/src/main/java/com/example/activity5/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.activity5
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
+import androidx.wear.widget.WearableLinearLayoutManager
 import com.example.activity5.databinding.ActivityMainBinding
+import com.example.activity5.note.Note
+import com.example.activity5.note.NoteAdapter
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -15,22 +18,7 @@ class MainActivity : Activity() {
     }
 
     private lateinit var binding: ActivityMainBinding
-
-    private fun testFirebase () {
-        // Testing get a document once
-        val docRef = NOTES.document("pl2ooxOWoQbWesSJfEbN")
-        docRef.get()
-            .addOnSuccessListener { document ->
-                if (document != null) {
-                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
-                } else {
-                    Log.d(TAG, "No such document")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.d(TAG, "get failed with ", exception)
-            }
-    }
+    private lateinit var adapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +26,34 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        testFirebase()
+        var listNotes = ArrayList<Note>()
+
+        NOTES.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    for (doc in document)
+                        listNotes.add(Note(
+                            doc.id,
+                            doc.data[MainActivityDescription.TITLE].toString(),
+                            doc.data[MainActivityDescription.DESCRIPTION].toString()
+                        ))
+                    // Set data to adapter and configurate
+                    adapter = NoteAdapter(listNotes, this)
+
+                    // Center childs elements in Recycler View
+                    binding.recyclerView.isEdgeItemsCenteringEnabled = true
+
+                    // When the display is circular, do an 180° effect in scroll
+                    binding.recyclerView.isCircularScrollingGestureEnabled = true
+
+                    binding.recyclerView.layoutManager = WearableLinearLayoutManager(this)
+                    binding.recyclerView.adapter = adapter
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
     }
 }
