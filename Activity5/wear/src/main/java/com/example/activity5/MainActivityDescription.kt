2ffbc8ed37b1402474220680043437104cf0386b
GandyA23 <avila.gandy@gmail.com>
2022-06-30 17:28:52 -0500

    Actividad 5 finalizada

--- Activity5/wear/src/main/java/com/example/activity5/MainActivityDescription.kt
+++ Activity5/wear/src/main/java/com/example/activity5/MainActivityDescription.kt
@@ -2,6 +2,7 @@ package com.example.activity5
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.activity5.databinding.ActivityMainDescriptionBinding
 
 class MainActivityDescription : Activity() {
@@ -19,5 +20,11 @@ class MainActivityDescription : Activity() {
         binding = ActivityMainDescriptionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val bundle : Bundle? = intent.extras
+        val title : TextView = binding.title
+        val description : TextView = binding.description
+
+        title.text = bundle?.getString(TITLE).toString()
+        description.text = bundle?.getString(DESCRIPTION).toString()
     }
 }
