3cbe18a29148309e5fb00b407b2a085d58a32e14
GandyA23 <avila.gandy@gmail.com>
2022-07-04 18:47:47 -0500

    Se agrega aplicación móvil al proyecto
    Se agrega la vista que agrega un documento a la colección en la aplicación.

--- Activity6AvilaGandy/mobile/src/main/java/com/example/activity6avilagandy/MainActivity.kt
+++ Activity6AvilaGandy/mobile/src/main/java/com/example/activity6avilagandy/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.activity6avilagandy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSend : Button = findViewById(R.id.btnSend)
+        val collectionRadio : RadioGroup = findViewById(R.id.collectionGroup);
+
+        collectionRadio.setOnCheckedChangeListener { group, checkedId -> {
+
+        }}
+
+        btnSend.setOnClickListener {
+            // Collect data from edittext
+            val title : EditText = findViewById(R.id.titleEditText)
+            val urlImage : EditText = findViewById(R.id.imageEditText)
+            val description : EditText = findViewById(R.id.descriptionEditText)
+        }
     }
 }
